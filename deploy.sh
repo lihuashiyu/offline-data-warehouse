377fb9fdf88079d0e161846dc54ebace19c15361
lihuashiyu <lihuashiyu@github.com>
2023-02-24 00:28:01 +0800

    1. 添加一件构建脚本； 2. 添加一键部署包生成脚本； 3. 修改部分配置文件；

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# =========================================================================================
+#    FileName      ：  deploy.sh
+#    CreateTime    ：  2023-02-23 23:07
+#    Author        ：  Issac_Al
+#    Email         ：  IssacAl@qq.com
+#    Description   ：  deploy.sh 被用于 ==> 对整体项目的构建，打包
+# =========================================================================================
+
+ROOT_DIR=$(cd $(dirname "$0") || exit; pwd)               # 项目根路径
+ALIAS_NAME="data-warehouse-1.0"                            # 项目别名
+LOG_FILE="deploy-$(date +%F-%H-%M-%S).log"                 # 操作日志存储
+PACKAGE_FILE=data-warehouse-1.0.tar.gz                     # 生成的部署包名
+
+
+# 1. 创建日志目录
+mkdir -p "${ROOT_DIR}/logs/"
+
+# 2. 构建 flume 源码包
+echo "============================ flume 源码构建 ============================"
+"${ROOT_DIR}/flume/build.sh" >> "${ROOT_DIR}/logs/${LOG_FILE}" 2>&1 
+sleep 1
+
+# 3.打包
+echo "============================= 部署包制作中 ============================="
+rm  "${ROOT_DIR}/deploy/${PACKAGE_FILE}"
+mv  "${ROOT_DIR}/deploy/" "${ROOT_DIR}/${ALIAS_NAME}"
+cd  "${ROOT_DIR}" || exit 
+tar -zcvf "${PACKAGE_FILE}" "${ALIAS_NAME}/" >> "${ROOT_DIR}/logs/${LOG_FILE}" 2>&1 
+sleep 1
+
+# 4. 清理退出
+echo "============================== 移动部署包 =============================="
+cd  "${ROOT_DIR}" || exit 
+mv  "${ROOT_DIR}/${PACKAGE_FILE}"  "${ROOT_DIR}/${ALIAS_NAME}/"
+mv  "${ROOT_DIR}/${ALIAS_NAME}"    "${ROOT_DIR}/deploy/"
+
+# 5. 退出
+echo "=============================== 完成退出 ==============================="
+exit 0
---
+++ flume/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# =========================================================================================
+#    FileName      ：  build.sh
+#    CreateTime    ：  2023-02-23 23:09
+#    Author        ：  Issac_Al
+#    Email         ：  IssacAl@qq.com
+#    Description   ：  build.sh 被用于 ==> flume 源码编译为 jar
+# =========================================================================================
+
+SERVICE_DIR=$(cd $(dirname "$0") || exit; pwd)             # 需要执行的服务路径
+ROOT_DIR=$(cd "${SERVICE_DIR}/../" || exit; pwd)           # 项目根路径
+MAVEN_HOME="${MAVEN_HOME}"                                 # Maven 安装目录
+LOG_FILE="flume-build-$(date +%F-%H-%M-%S).log"            # 操作日志存储
+
+# 1. 创建日志目录 logs
+mkdir -p "${ROOT_DIR}/logs/" 
+
+# 2. 切换到源码路径
+cd "${SERVICE_DIR}" || exit
+
+# 3. 使用 Maven 进行构建
+echo "============================ flume 源码构建 ==========================="
+"${MAVEN_HOME}/bin/mvn" clean package -DskipTests=true >> "${ROOT_DIR}/logs/${LOG_FILE}" 2>&1
+
+# 4. 复制生成的 jar 到 deploy/file-kafka
+echo "============================== 复制 jar =============================="
+cp -fp "${SERVICE_DIR}"/target/*with*.jar "${ROOT_DIR}"/deploy/file-kafka/flume-1.0.jar
+
+# 5. 清理退出 
+echo "============================== 清理退出 =============================="
+"${MAVEN_HOME}/bin/mvn" clean >> "${ROOT_DIR}/logs/${LOG_FILE}" 2>&1 
+exit 0
