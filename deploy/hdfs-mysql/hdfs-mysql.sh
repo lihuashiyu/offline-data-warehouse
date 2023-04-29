c8f92a33d99ac88c2c3c0a752db3fb0ff261ef20
lihuashiyu <lihuashiyu@github.com>
2023-04-29 18:12:00 +0800

    1. 添加标准化日志输出 2. 添加脚本提示 3. 修复获取 mysql 表变量

--- deploy/hdfs-mysql/hdfs-mysql.sh
+++ deploy/hdfs-mysql/hdfs-mysql.sh
@@ -14,7 +14,7 @@ HADOOP_DIR=/opt/apache/hadoop                              # Hadoop 路径
 SERVICE_DIR=$(cd "$(dirname "$0")" || exit; pwd)           # 程序位置
 WAREHOUSE_DIR=/warehouse/ads                               # HDFS 的路径
 MYSQL_DATA_BASE=view_report                                # Mysql 数据库名
-LOG_FILE=hdfs_mysql.log                                    # 操作日志
+LOG_FILE="hdfs-mysql-$(date +%F).log"                      # 操作日志
 
 
 # DataX 导出路径不允许存在空文件，该函数作用为清理空文件
@@ -24,83 +24,98 @@ function export_data()
     data_dir_list=$("${HADOOP_DIR}/bin/hadoop" fs -ls -R "$2" | awk '{print $8}')
     for path in ${data_dir_list}
     do
-        # DataX导出路径不允许存在空文件，该函数作用为清理空文件
+        # DataX 导出路径不允许存在空文件，该函数作用为清理空文件
         "${HADOOP_DIR}/bin/hadoop" fs -test -z "${path}"
         if [[ $? -eq 0 ]]; then
-            echo "    路径（${path}）中的文件大小为0，正在删除 ...... "
-            "${HADOOP_DIR}/bin/hadoop" fs -rm -r -f "${path}"  >> "${SERVICE_DIR}/${LOG_FILE}" 2>&1
+            echo "    路径（${path}）中的文件大小为 0，正在删除 ...... "
+            "${HADOOP_DIR}/bin/hadoop" fs -rm -r -f "${path}"  >> "${SERVICE_DIR}/logs/${LOG_FILE}" 2>&1
         fi
     done
     
     # 执行计划
-    echo "    DataX 正在同步 HDFS（$2）的文件到 Mysql（${MYSQL_DATA_BASE}.$(echo $1 | awk -F '[/.]' '{print $4}')）数据库 ...... "
-    /usr/bin/python3 "${DATAX_DIR}/bin/datax.py" -p "-Dexportdir=$2" "$1"  >> "${SERVICE_DIR}/${LOG_FILE}" 2>&1
+    echo "    DataX 正在同步 HDFS（$2）的文件到 Mysql 表（${MYSQL_DATA_BASE}.$(echo $1 | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}')） ...... "
+    /usr/bin/env python3 "${DATAX_DIR}/bin/datax.py" -p "-Dexportdir=$2" "$1"  >> "${SERVICE_DIR}/logs/${LOG_FILE}" 2>&1
 }
 
 
 printf "\n=================================== 运行开始 ===================================\n"
 case $1 in
-    "ads_new_buyer_stats")
+    # 新增交易用户统计
+    ads_new_buyer_stats)
         export_data "${SERVICE_DIR}/conf/ads_new_buyer_stats.json" "${WAREHOUSE_DIR}/ads_new_buyer_stats"
     ;;
     
-    "ads_order_by_province")
+    # 各省份交易统计
+    ads_order_by_province)
         export_data "${SERVICE_DIR}/conf/ads_order_by_province.json" "${WAREHOUSE_DIR}/ads_order_by_province"
     ;;
     
-    "ads_page_path")
+    # 路径分析(页面单跳)
+    ads_page_path)
         export_data "${SERVICE_DIR}/conf/ads_page_path.json" "${WAREHOUSE_DIR}/ads_page_path"
     ;;
     
-    "ads_repeat_purchase_by_tm")
+    # 最近7/30日各品牌复购率 
+    ads_repeat_purchase_by_tm)
         export_data "${SERVICE_DIR}/conf/ads_repeat_purchase_by_tm.json" "${WAREHOUSE_DIR}/ads_repeat_purchase_by_tm"
     ;;
     
-    "ads_trade_stats")
+    # 交易综合统计  
+    ads_trade_stats)
         export_data "${SERVICE_DIR}/conf/ads_trade_stats.json" "${WAREHOUSE_DIR}/ads_trade_stats"
     ;;
     
-    "ads_trade_stats_by_cate")
+    # 各品类商品交易统计
+    ads_trade_stats_by_cate)
         export_data "${SERVICE_DIR}/conf/ads_trade_stats_by_cate.json" "${WAREHOUSE_DIR}/ads_trade_stats_by_cate"
     ;;
     
-    "ads_trade_stats_by_tm")
+    # 各品牌商品交易统计
+    ads_trade_stats_by_tm)
         export_data "${SERVICE_DIR}/conf/ads_trade_stats_by_tm.json" "${WAREHOUSE_DIR}/ads_trade_stats_by_tm"
     ;;
     
-    "ads_traffic_stats_by_channel")
+    # 各渠道流量统计
+    ads_traffic_stats_by_channel)
         export_data "${SERVICE_DIR}/conf/ads_traffic_stats_by_channel.json" "${WAREHOUSE_DIR}/ads_traffic_stats_by_channel"
     ;;
     
-    "ads_user_action")
+    # 用户行为漏斗分析
+    ads_user_action)
         export_data "${SERVICE_DIR}/conf/ads_user_action.json" "${WAREHOUSE_DIR}/ads_user_action"
     ;;
     
-    "ads_user_change")
+    # 用户变动统计
+    ads_user_change)
         export_data "${SERVICE_DIR}/conf/ads_user_change.json" "${WAREHOUSE_DIR}/ads_user_change"
     ;;
     
-    "ads_user_retention")
+    # 用户留存率
+    ads_user_retention)
         export_data "${SERVICE_DIR}/conf/ads_user_retention.json" "${WAREHOUSE_DIR}/ads_user_retention"
     ;;
     
-    "ads_user_stats")
+    # 用户新增活跃统计
+    ads_user_stats)
         export_data "${SERVICE_DIR}/conf/ads_user_stats.json" "${WAREHOUSE_DIR}/ads_user_stats"
     ;;
     
-    "ads_activity_stats")
+    # 最近 30 天发布的活动的补贴率
+    ads_activity_stats)
         export_data "${SERVICE_DIR}/conf/ads_activity_stats.json" "${WAREHOUSE_DIR}/ads_activity_stats"
     ;;
     
-    "ads_coupon_stats")
+    # 最近 30 天发布的优惠券的补贴率
+    ads_coupon_stats)
         export_data "${SERVICE_DIR}/conf/ads_coupon_stats.json" "${WAREHOUSE_DIR}/ads_coupon_stats"
     ;;
     
-    "ads_sku_cart_num_top3_by_cate")
+    # 各分类商品购物车存量 top3
+    ads_sku_cart_num_top3_by_cate)
         export_data "${SERVICE_DIR}/conf/ads_sku_cart_num_top3_by_cate.json" "${WAREHOUSE_DIR}/ads_sku_cart_num_top3_by_cate"
     ;;
     
-    "all")
+    all)
         export_data "${SERVICE_DIR}/conf/ads_new_buyer_stats.json"           "${WAREHOUSE_DIR}/ads_new_buyer_stats"
         export_data "${SERVICE_DIR}/conf/ads_order_by_province.json"         "${WAREHOUSE_DIR}/ads_order_by_province"
         export_data "${SERVICE_DIR}/conf/ads_page_path.json"                 "${WAREHOUSE_DIR}/ads_page_path"
@@ -116,6 +131,31 @@ case $1 in
         export_data "${SERVICE_DIR}/conf/ads_activity_stats.json"            "${WAREHOUSE_DIR}/ads_activity_stats"
         export_data "${SERVICE_DIR}/conf/ads_coupon_stats.json"              "${WAREHOUSE_DIR}/ads_coupon_stats"
         export_data "${SERVICE_DIR}/conf/ads_sku_cart_num_top3_by_cate.json" "${WAREHOUSE_DIR}/ads_sku_cart_num_top3_by_cate"
+    ;;
+    
+    *)
+        export_data "${SERVICE_DIR}/conf/ads_new_buyer_stats.json"           "${WAREHOUSE_DIR}/ads_new_buyer_stats"
+        echo "    脚本可传入一个参数，如下所示：                                       "
+        echo "        +-------------------------------+--------------------------------+ "
+        echo "        |             参 数             |             描  述             | "
+        echo "        +-------------------------------+--------------------------------+ "
+        echo "        | ads_activity_stats            | 最近 30 天发布的活动的补贴率   | "
+        echo "        | ads_coupon_stats              | 最近 30 天发布的优惠券的补贴率 | "
+        echo "        | ads_new_buyer_stats           | 新增交易用户统计               | "
+        echo "        | ads_order_by_province         | 各省份交易统计                 | "
+        echo "        | ads_page_path                 | 路径分析(页面单跳)             | "
+        echo "        | ads_repeat_purchase_by_tm     | 最近 7/30 日各品牌复购率       | "
+        echo "        | ads_sku_cart_num_top3_by_cate | 各分类商品购物车存量 Top3      | "
+        echo "        | ads_trade_stats_by_cate       | 各品类商品交易统计             | "
+        echo "        | ads_trade_stats_by_tm         | 各品牌商品交易统计             | "
+        echo "        | ads_trade_stats               | 交易综合统计                   | "
+        echo "        | ads_traffic_stats_by_channel  | 各渠道流量统计                 | "
+        echo "        | ads_user_action               | 用户行为漏斗分析               | "
+        echo "        | ads_user_change               | 用户变动统计                   | "
+        echo "        | ads_user_retention            | 用户留存率                     | "
+        echo "        | ads_user_stats                | 用户新增活跃统计               | "
+        echo "        | all                           | 全部 ads 表                    | "   
+        echo "        +-------------------------------+--------------------------------+ "
     ;;
 esac
 printf "=================================== 运行结束 ===================================\n\n"
--- deploy/mysql-hdfs/mysql-hdfs.sh
+++ deploy/mysql-hdfs/mysql-hdfs.sh
@@ -12,10 +12,10 @@
     
 SERVICE_DIR=$(cd "$(dirname "$0")" || exit; pwd)           # 程序位置
 DATAX_HOME=/opt/github/datax                               # Datax 安装路径
-HADOOP_HOME=/opt/apache/hadoop                             # Hadoop 路径
+HADOOP_HOME=/opt/apache/hadoop                             # Hadoop 安装路径
 WAREHOUSE_DIR=/warehouse/db                                # HDFS 上的路径
 LOG_FILE="mysql-hdfs-$(date +%F).log"                      # 操作日志
-MYSQL_DATA_BASE="at_gui_gu"                                #  Mysql 的数据库名称
+MYSQL_DATA_BASE="at_gui_gu"                                # Mysql 的数据库名称
 do_date=$(date -d "-1 day" +%F)                            # 当前日期的前一天
 
 # 如果传入日期则 do_date 等于传入的日期，否则等于前一天日期
@@ -37,81 +37,97 @@ import_data()
     else
         cs=$("${HADOOP_HOME}/bin/hadoop" fs -count "$2" | awk '{print $3}')
         if [[ "${cs}" -ne 0 ]]; then
-            echo "    路径（$2）不为空，正在清空......"
+            echo "    路径（$2）不为空，正在清空 ...... "
             ${HADOOP_HOME}/bin/hadoop fs -rm -r -f "$2/*"  >> "${SERVICE_DIR}/logs/${LOG_FILE}" 2>&1
         fi
     fi
     
     # 3. 执行计划，进行数据同步
-    echo "    DataX 正在同步表（${MYSQL_DATA_BASE}.$(echo $1 | awk -F '[/.]' '{print $4}')）的数据到 HDFS 的 $2 路径 ...... "
-    /usr/bin/python3 "${DATAX_HOME}/bin/datax.py" -p "-Dtargetdir=$2" "$1"  >> "${SERVICE_DIR}/logs/${LOG_FILE}" 2>&1
+    echo "    DataX 正在同步表（${MYSQL_DATA_BASE}.$(echo $1 | awk -F '/' '{print $4}' | awk -F '.' '{print $1}')）的数据到 HDFS（$2）路径 ...... "
+    /usr/bin/env python3 "${DATAX_HOME}/bin/datax.py" -p "-Dtargetdir=$2" "$1"  >> "${SERVICE_DIR}/logs/${LOG_FILE}" 2>&1
 }
 
 
 printf "\n=================================== 运行开始 ===================================\n"
 # 导出数据
 case $1 in
-    "activity_info")
+    # 活动信息表
+    activity_info)
         import_data "${SERVICE_DIR}/conf/activity_info.json" "${WAREHOUSE_DIR}/activity_info/${do_date}"
     ;;
-        
-    "activity_rule")
+    
+    # 活动规则表
+    activity_rule)
         import_data "${SERVICE_DIR}/conf/activity_rule.json" "${WAREHOUSE_DIR}/activity_rule_full/${do_date}"
     ;;
-        
-    "base_category1")
+       
+    # 一级分类表
+    base_category1)
         import_data "${SERVICE_DIR}/conf/base_category1.json" "${WAREHOUSE_DIR}/base_category1_full/${do_date}"
     ;;
-        
-    "base_category2")
+    
+    # 二级分类表    
+    base_category2)
         import_data "${SERVICE_DIR}/conf/base_category2.json" "${WAREHOUSE_DIR}/base_category2_full/${do_date}"
     ;;
-        
-    "base_category3")
+    
+    # 三级分类表    
+    base_category3)
         import_data "${SERVICE_DIR}/conf/base_category3.json" "${WAREHOUSE_DIR}/base_category3_full/${do_date}"
     ;;
-        
-    "base_dic")
+    
+    # 字典表    
+    base_dic)
         import_data "${SERVICE_DIR}/conf/base_dic.json" "${WAREHOUSE_DIR}/base_dic_full/${do_date}"
     ;;
-        
-    "base_province")
+       
+    # 省份表 
+    base_province)
         import_data "${SERVICE_DIR}/conf/base_province.json" "${WAREHOUSE_DIR}/base_province_full/${do_date}"
     ;;
         
-    "base_region")
+    # 地区表
+    base_region)
         import_data "${SERVICE_DIR}/conf/base_region.json" "${WAREHOUSE_DIR}/base_region_full/${do_date}"
     ;;
         
-    "base_trademark")
+    # 品牌表
+    base_trademark)
         import_data "${SERVICE_DIR}/conf/base_trademark.json" "${WAREHOUSE_DIR}/base_trademark_full/${do_date}"
-    ;;
-        
-    "cart_info")
+    ;;        
+      
+    # 购物车表 
+    cart_info)
         import_data "${SERVICE_DIR}/conf/cart_info.json" "${WAREHOUSE_DIR}/cart_info_full/${do_date}"
     ;;
         
-    "coupon_info")
+    # 优惠券信息
+    coupon_info)
         import_data "${SERVICE_DIR}/conf/coupon_info.json" "${WAREHOUSE_DIR}/coupon_info_full/${do_date}"
     ;;
         
-    "sku_attr_value")
+    # SKU 平台属性值表
+    sku_attr_value)
         import_data "${SERVICE_DIR}/conf/sku_attr_value.json" "${WAREHOUSE_DIR}/sku_attr_value_full/${do_date}"
     ;;
-        
-    "sku_info")
+    
+    # SKU 信息表
+    sku_info)
         import_data "${SERVICE_DIR}/conf/sku_info.json" "${WAREHOUSE_DIR}/sku_info_full/${do_date}"
     ;;
         
-    "sku_sale_attr_value")
+    # SKU 销售属性表
+    sku_sale_attr_value)
         import_data "${SERVICE_DIR}/conf/sku_sale_attr_value.json" "${WAREHOUSE_DIR}/sku_sale_attr_value_full/${do_date}"
     ;;
-        
-    "spu_info")
+       
+    # SPU 信息表 
+    spu_info)
         import_data "${SERVICE_DIR}/conf/spu_info.json" "${WAREHOUSE_DIR}/spu_info_full/${do_date}"
     ;;
-        
-    "all")
+    
+    # 全部 15 张表    
+    all)
         import_data "${SERVICE_DIR}/conf/activity_info.json"       "${WAREHOUSE_DIR}/activity_info_full/${do_date}"
         import_data "${SERVICE_DIR}/conf/activity_rule.json"       "${WAREHOUSE_DIR}/activity_rule_full/${do_date}"
         import_data "${SERVICE_DIR}/conf/base_category1.json"      "${WAREHOUSE_DIR}/base_category1_full/${do_date}"
@@ -127,6 +143,30 @@ case $1 in
         import_data "${SERVICE_DIR}/conf/sku_info.json"            "${WAREHOUSE_DIR}/sku_info_full/${do_date}"
         import_data "${SERVICE_DIR}/conf/sku_sale_attr_value.json" "${WAREHOUSE_DIR}/sku_sale_attr_value_full/${do_date}"
         import_data "${SERVICE_DIR}/conf/spu_info.json"            "${WAREHOUSE_DIR}/spu_info_full/${do_date}"
+    ;;
+    
+    *)
+        echo "    脚本可传入一个参数，如下所示：                    "
+        echo "        +----------------------+------------------+ "
+        echo "        |        参  数        |      描  述      | "
+        echo "        +----------------------+------------------+ "
+        echo "        | activity_info        | 活动信息表       | "   
+        echo "        | activity_rule        | 活动规则表       | "     
+        echo "        | base_category1       | 一级分类表       | "     
+        echo "        | base_category2       | 二级分类表       | "     
+        echo "        | base_category3       | 三级分类表       | "     
+        echo "        | base_dic             | 字典表           | "     
+        echo "        | base_province        | 省份表           | "       
+        echo "        | base_region          | 地区表           | "       
+        echo "        | base_trademark       | 品牌表           | "       
+        echo "        | cart_info            | 购物车表         | "      
+        echo "        | coupon_info          | 优惠券信息       | "     
+        echo "        | sku_attr_value       | SKU 平台属性值表 | " 
+        echo "        | sku_info             | SKU 信息表       | "   
+        echo "        | sku_sale_attr_value  | SKU 销售属性表   | " 
+        echo "        | spu_info             | SPU 信息表       | "   
+        echo "        | all                  | 全部 mysql 表    | "   
+        echo "        +----------------------+------------------+ "
     ;;
 esac
 printf "=================================== 运行结束 ===================================\n\n"
